7d3172cf4018701e64440e2bafdb4fc1e33db48d
Loitp <loitp@pateco.vn>
2018-05-02 11:14:48 +0700

    update

--- uizacoresdk/src/main/java/vn/loitp/uizavideo/view/UizaDialogShare.java
+++ uizacoresdk/src/main/java/vn/loitp/uizavideo/view/UizaDialogShare.java
@@ -3,15 +3,11 @@ package vn.loitp.uizavideo.view;
 import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
-import android.support.v4.app.ShareCompat;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 
-import com.daimajia.androidanimations.library.Techniques;
-
 import loitp.core.R;
-import vn.loitp.core.utilities.LAnimationUtil;
 import vn.loitp.core.utilities.LSocialUtil;
 
 /**
@@ -27,7 +23,7 @@ public class UizaDialogShare extends Dialog implements android.view.View.OnClick
 
     private ImageView btFb;
     private ImageView btInstagram;
-    private ImageView bt_twiter;
+    private ImageView btTwiter;
 
     public UizaDialogShare(Activity activity) {
         super(activity);
@@ -41,11 +37,11 @@ public class UizaDialogShare extends Dialog implements android.view.View.OnClick
         setContentView(R.layout.dialog_share);
         btFb = (ImageView) findViewById(R.id.bt_fb);
         btInstagram = (ImageView) findViewById(R.id.bt_instagram);
-        bt_twiter = (ImageView) findViewById(R.id.bt_twiter);
+        btTwiter = (ImageView) findViewById(R.id.bt_twiter);
 
         btFb.setOnClickListener(this);
         btInstagram.setOnClickListener(this);
-        bt_twiter.setOnClickListener(this);
+        btTwiter.setOnClickListener(this);
     }
 
     @Override
@@ -54,7 +50,7 @@ public class UizaDialogShare extends Dialog implements android.view.View.OnClick
             onClickFb();
         } else if (v == btInstagram) {
             onClickInstagram();
-        } else if (v == bt_twiter) {
+        } else if (v == btTwiter) {
             onClickTwiter();
         }
         dismiss();
