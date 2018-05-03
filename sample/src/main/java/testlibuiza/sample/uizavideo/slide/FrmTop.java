5b2e0e3808bb90b7ceb3d87b2d29a8c1990059d8
Loitp <loitp@pateco.vn>
2018-05-03 15:52:42 +0700

    update playlist

---
+++ sample/src/main/java/testlibuiza/sample/uizavideo/slide/FrmTop.java
@@ -0,0 +1,23 @@
+package testlibuiza.sample.uizavideo.slide;
+
+/**
+ * Created by www.muathu@gmail.com on 12/24/2017.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import testlibuiza.R;
+import vn.loitp.core.base.BaseFragment;
+
+public class FrmTop extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frm_top, container, false);
+    }
+}
--- sample/src/main/java/testlibuiza/sample/uizavideo/slide/TestUizaVideoIMActivityRlSlide.java
+++ sample/src/main/java/testlibuiza/sample/uizavideo/slide/TestUizaVideoIMActivityRlSlide.java
@@ -1,15 +1,44 @@
 package testlibuiza.sample.uizavideo.slide;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import testlibuiza.R;
 import vn.loitp.core.base.BaseActivity;
+import vn.loitp.core.utilities.LLog;
+import vn.loitp.views.layout.draggablepanel.DraggableListener;
+import vn.loitp.views.layout.draggablepanel.DraggablePanel;
 
 public class TestUizaVideoIMActivityRlSlide extends BaseActivity {
+    private DraggablePanel draggablePanel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        draggablePanel = (DraggablePanel) findViewById(R.id.draggable_panel);
+        initializeDraggablePanel();
+
+        draggablePanel.setDraggableListener(new DraggableListener() {
+            @Override
+            public void onMaximized() {
+                LLog.d(TAG, "onMaximized");
+            }
+
+            @Override
+            public void onMinimized() {
+                LLog.d(TAG, "onMinimized");
+            }
+
+            @Override
+            public void onClosedToLeft() {
+                LLog.d(TAG, "onClosedToLeft");
+            }
+
+            @Override
+            public void onClosedToRight() {
+                LLog.d(TAG, "onClosedToRight");
+            }
+        });
     }
 
     @Override
@@ -25,5 +54,24 @@ public class TestUizaVideoIMActivityRlSlide extends BaseActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.test_uiza_ima_video_activity_rl_slide;
+    }
+
+    private void initializeDraggablePanel() throws Resources.NotFoundException {
+        FrmTop frmTop = new FrmTop();
+        FrmBottom frmBottom = new FrmBottom();
+
+        draggablePanel.setFragmentManager(getSupportFragmentManager());
+        draggablePanel.setTopFragment(frmTop);
+        draggablePanel.setBottomFragment(frmBottom);
+
+        //draggablePanel.setXScaleFactor(xScaleFactor);
+        //draggablePanel.setYScaleFactor(yScaleFactor);
+        draggablePanel.setTopViewHeight(800);
+        //draggablePanel.setTopFragmentMarginRight(topViewMarginRight);
+        //draggablePanel.setTopFragmentMarginBottom(topViewMargnBottom);
+        draggablePanel.setClickToMaximizeEnabled(false);
+        draggablePanel.setClickToMinimizeEnabled(false);
+
+        draggablePanel.initializeView();
     }
 }
