76c5afd28d2ef675ff527c57936d41907bf35ae2
Loitp <loitp@pateco.vn>
2018-05-03 11:00:00 +0700

    update sample

--- uizacoresdk/src/main/java/vn/loitp/restapi/restclient/RestClientTracking.java
+++ uizacoresdk/src/main/java/vn/loitp/restapi/restclient/RestClientTracking.java
@@ -6,16 +6,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.security.InvalidParameterException;
-import java.security.cert.CertificateException;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
