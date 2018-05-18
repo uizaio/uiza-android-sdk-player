ba6f1123935145bedac00e8331c1891f613bd6b9
Loitp <loitp@pateco.vn>
2018-05-18 16:18:06 +0700

    update

--- uizacoresdk/src/main/java/vn/loitp/uizavideo/view/rl/video/UizaIMAVideo.java
+++ uizacoresdk/src/main/java/vn/loitp/uizavideo/view/rl/video/UizaIMAVideo.java
@@ -242,18 +242,14 @@ public class UizaIMAVideo extends RelativeLayout implements PreviewView.OnPrevie
                 final List<Subtitle> subtitleList = mGetDetailEntity.getData().get(0).getSubtitle();
                 //LLog.d(TAG, "subtitleList toJson: " + gson.toJson(subtitleList));
 
-                activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (subtitleList == null || subtitleList.isEmpty()) {
-                            exoCc.setVisibility(View.GONE);
-                            LLog.d(TAG, "exoCc GONE");
-                        } else {
-                            exoCc.setVisibility(View.VISIBLE);
-                            LLog.d(TAG, "exoCc VISIBLE");
-                        }
-                    }
-                });
+
+                if (subtitleList == null || subtitleList.isEmpty()) {
+                    exoCc.setVisibility(View.GONE);
+                    LLog.d(TAG, "exoCc GONE");
+                } else {
+                    exoCc.setVisibility(View.VISIBLE);
+                    LLog.d(TAG, "exoCc VISIBLE");
+                }
 
                 initData(linkPlay, UizaData.getInstance().getUizaInput().getUrlIMAAd(), UizaData.getInstance().getUizaInput().getUrlThumnailsPreviewSeekbar(), subtitleList);
                 onResume();
