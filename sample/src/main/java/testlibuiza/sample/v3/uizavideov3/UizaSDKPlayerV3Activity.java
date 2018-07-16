5db22215686a9f867518332792fa6d90c2ba6231
Loitp <loitp@pateco.vn>
2018-07-16 16:04:36 +0700

    update

--- sample/src/main/java/testlibuiza/sample/v3/uizavideov3/UizaSDKPlayerV3Activity.java
+++ sample/src/main/java/testlibuiza/sample/v3/uizavideov3/UizaSDKPlayerV3Activity.java
@@ -9,12 +9,22 @@ import android.widget.EditText;
 
 import testlibuiza.R;
 import vn.loitp.core.base.BaseActivity;
+import vn.loitp.core.common.Constants;
 import vn.loitp.core.utilities.LUIUtil;
+import vn.loitp.uizavideov3.view.util.UizaDataV3;
 
 public class UizaSDKPlayerV3Activity extends BaseActivity {
 
     private EditText etInputEntityId;
     private Button btStart;
+
+    //change your infor
+    private final String currentPlayerId = Constants.PLAYER_ID_SKIN_1;
+    private final String domainTracking = Constants.URL_TRACKING_STAG;
+    private final String DF_DOMAIN_API = "android-api.uiza.co";
+    private final String DF_TOKEN = "uap-16f8e65d8e2643ffa3ff5ee9f4f9ba03-a07716a6";
+    private final String DF_APP_ID = "16f8e65d8e2643ffa3ff5ee9f4f9ba03";
+    private final String entityIdDefault = "61d4b1e2-95d9-418d-9e52-e1d798e869b4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +34,8 @@ public class UizaSDKPlayerV3Activity extends BaseActivity {
         etInputEntityId = (EditText) findViewById(R.id.et_input_entity_id);
         btStart = (Button) findViewById(R.id.bt_start);
 
-        //init workspace
-        initWorkscape();
-
         //set default value entity id
-        etInputEntityId.setText("ffff");
+        etInputEntityId.setText(entityIdDefault);
         LUIUtil.setLastCursorEditText(etInputEntityId);
 
         etInputEntityId.addTextChangedListener(new TextWatcher() {
@@ -53,7 +60,21 @@ public class UizaSDKPlayerV3Activity extends BaseActivity {
         btStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                UizaDataV3.getInstance().setCurrentPlayerId(currentPlayerId);
+                UizaDataV3.getInstance().initTracking(domainTracking);
+                UizaDataV3.getInstance().initSDK(DF_DOMAIN_API, DF_TOKEN, DF_APP_ID);
 
+                /*final Intent intent = new Intent(activity, HomeV3CanSlideActivity.class);
+                if (intent != null) {
+                    LUIUtil.setDelay(3000, new LUIUtil.DelayCallback() {
+                        @Override
+                        public void doAfter(int mls) {
+                            startActivity(intent);
+                            LActivityUtil.tranIn(activity);
+                            finish();
+                        }
+                    });
+                }*/
             }
         });
     }
@@ -71,9 +92,5 @@ public class UizaSDKPlayerV3Activity extends BaseActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.v3_player_input_entity_id_activity;
-    }
-
-    private void initWorkscape() {
-
     }
 }
