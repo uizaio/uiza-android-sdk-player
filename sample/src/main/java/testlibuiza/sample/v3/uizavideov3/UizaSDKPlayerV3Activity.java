757a9fadc59705b320c91c2c9b6f5733ea427d78
Loitp <loitp@pateco.vn>
2018-07-16 15:56:49 +0700

    update

--- sample/src/main/java/testlibuiza/sample/v3/uizavideov3/UizaSDKPlayerV3Activity.java
+++ sample/src/main/java/testlibuiza/sample/v3/uizavideov3/UizaSDKPlayerV3Activity.java
@@ -1,16 +1,61 @@
 package testlibuiza.sample.v3.uizavideov3;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import testlibuiza.R;
 import vn.loitp.core.base.BaseActivity;
+import vn.loitp.core.utilities.LUIUtil;
 
 public class UizaSDKPlayerV3Activity extends BaseActivity {
+
+    private EditText etInputEntityId;
+    private Button btStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //find views
+        etInputEntityId = (EditText) findViewById(R.id.et_input_entity_id);
+        btStart = (Button) findViewById(R.id.bt_start);
+
+        //init workspace
+        initWorkscape();
+
+        //set default value entity id
+        etInputEntityId.setText("ffff");
+        LUIUtil.setLastCursorEditText(etInputEntityId);
+
+        etInputEntityId.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s == null || s.toString().isEmpty()) {
+                    btStart.setEnabled(false);
+                } else {
+                    btStart.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        btStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
@@ -26,5 +71,9 @@ public class UizaSDKPlayerV3Activity extends BaseActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.v3_player_input_entity_id_activity;
+    }
+
+    private void initWorkscape() {
+
     }
 }
