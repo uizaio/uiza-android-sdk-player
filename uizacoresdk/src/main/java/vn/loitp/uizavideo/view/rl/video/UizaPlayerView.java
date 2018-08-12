1272443fcc4448fcaea72d35050e085bf31d22f7
Loitp <loitp@pateco.vn>
2018-08-12 14:44:22 +0700

    update demo

--- uizacoresdk/src/main/java/vn/loitp/uizavideo/view/rl/video/UizaPlayerView.java
+++ uizacoresdk/src/main/java/vn/loitp/uizavideo/view/rl/video/UizaPlayerView.java
@@ -60,13 +60,13 @@ public final class UizaPlayerView extends PlayerView implements PlayerControlVie
                 if (tapStartTimeMs != 0) {
                     if (SystemClock.elapsedRealtime() - tapStartTimeMs < LONG_PRESS_THRESHOLD_MS) {
                         if (!controllerVisible) {
-                            LLog.d(TAG, "showController");
+                            //LLog.d(TAG, "showController");
                             showController();
                             if (controllerStateCallback != null) {
                                 controllerStateCallback.onVisibilityChange(true);
                             }
                         } else if (getControllerHideOnTouch()) {
-                            LLog.d(TAG, "hideController");
+                            //LLog.d(TAG, "hideController");
                             hideController();
                             if (controllerStateCallback != null) {
                                 controllerStateCallback.onVisibilityChange(false);
