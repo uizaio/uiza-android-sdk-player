fb0772d2e58bff19d34aff53bd37e2a9a433e474
Loitp <loitp@pateco.vn>
2018-09-06 14:25:12 +0700

    update core sdk

--- demouiza/src/main/java/uiza/app/LSApplication.java
+++ demouiza/src/main/java/uiza/app/LSApplication.java
@@ -7,13 +7,17 @@ import com.google.gson.Gson;
 
 import vn.loitp.core.common.Constants;
 import vn.loitp.data.ActivityData;
+import vn.loitp.uizavideov3.util.UizaDataV3;
 import vn.loitp.utils.util.Utils;
 
 public class LSApplication extends MultiDexApplication {
     private final String TAG = LSApplication.class.getSimpleName();
     private static LSApplication instance;
     private Gson gson;
-    protected String userAgent = Constants.USER_AGENT;
+    private final String DF_DOMAIN_API = "loctbprod01.uiza.co";
+    private final String DF_TOKEN = "uap-9816792bb84642f09d843af4f93fb748-b94fcbd1";
+    private final String DF_APP_ID = "9816792bb84642f09d843af4f93fb748";
+    private int environment = Constants.ENVIRONMENT_PROD;
 
     @Override
     public void onCreate() {
@@ -31,6 +35,8 @@ public class LSApplication extends MultiDexApplication {
             return;
         }
         LeakCanary.install(this);*/
+
+        UizaDataV3.getInstance().initSDK(DF_DOMAIN_API, DF_TOKEN, DF_APP_ID, environment);
     }
 
     public Gson getGson() {
