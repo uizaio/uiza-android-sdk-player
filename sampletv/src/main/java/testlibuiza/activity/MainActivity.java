a5b16f54fb27bb1833f4d3c74623f2a4ca4bd863
Loitp <loitp@pateco.vn>
2018-09-18 15:08:31 +0700

    update sample tv mainscreen ok

--- sampletv/src/main/java/testlibuiza/activity/MainActivity.java
+++ sampletv/src/main/java/testlibuiza/activity/MainActivity.java
@@ -1,18 +1,23 @@
 package testlibuiza.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import testlibuiza.app.LSApplication;
 import testlibuiza.app.R;
 import vn.uiza.core.base.BaseActivity;
 import vn.uiza.core.utilities.LLog;
+import vn.uiza.utils.util.KeyboardUtils;
+import vn.uiza.views.LToast;
 
-public class MainActivity extends BaseActivity implements View.OnFocusChangeListener {
+public class MainActivity extends BaseActivity implements View.OnFocusChangeListener, View.OnClickListener {
     private EditText etInput;
     private Button btVod;
     private Button btLive;
+    private Button btClear;
     private Button btStart;
 
     @Override
@@ -21,12 +26,25 @@ public class MainActivity extends BaseActivity implements View.OnFocusChangeList
         etInput = (EditText) findViewById(R.id.et_input);
         btVod = (Button) findViewById(R.id.bt_vod);
         btLive = (Button) findViewById(R.id.bt_live);
+        btClear = (Button) findViewById(R.id.bt_clear);
         btStart = (Button) findViewById(R.id.bt_start);
 
         etInput.setOnFocusChangeListener(this);
         btVod.setOnFocusChangeListener(this);
         btLive.setOnFocusChangeListener(this);
+        btClear.setOnFocusChangeListener(this);
         btStart.setOnFocusChangeListener(this);
+
+        updateUI(etInput, true);
+        updateUI(btVod, false);
+        updateUI(btLive, false);
+        updateUI(btClear, false);
+        updateUI(btStart, false);
+
+        btVod.setOnClickListener(this);
+        btLive.setOnClickListener(this);
+        btClear.setOnClickListener(this);
+        btStart.setOnClickListener(this);
     }
 
     @Override
@@ -45,15 +63,46 @@ public class MainActivity extends BaseActivity implements View.OnFocusChangeList
     }
 
     @Override
-    public void onFocusChange(View view, boolean b) {
+    public void onFocusChange(View view, boolean isFocus) {
+        updateUI(view, isFocus);
         if (view == etInput) {
-            LLog.d(TAG, "onFocusChange etInput");
-        } else if (view == btVod) {
-            LLog.d(TAG, "onFocusChange btVod");
+            LLog.d(TAG, "onFocusChange etInput " + isFocus);
+            if (isFocus) {
+                etInput.setTextColor(Color.BLACK);
+                etInput.setHintTextColor(Color.BLACK);
+            } else {
+                etInput.setTextColor(Color.WHITE);
+                etInput.setHintTextColor(Color.WHITE);
+            }
+            KeyboardUtils.toggleSoftInput();
+        }
+    }
+
+    private void updateUI(View view, boolean isFocus) {
+        if (view == null) {
+            return;
+        }
+        if (isFocus) {
+            view.setBackgroundResource(R.drawable.bkg_has_focus);
+        } else {
+            view.setBackgroundResource(R.drawable.bkg_no_focus);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btVod) {
+            etInput.setText(LSApplication.entityIdDefaultVOD);
         } else if (view == btLive) {
-            LLog.d(TAG, "onFocusChange btLive");
+            etInput.setText(LSApplication.entityIdDefaultLIVE);
+        } else if (view == btClear) {
+            etInput.setText("");
         } else if (view == btStart) {
-            LLog.d(TAG, "onFocusChange btStart");
+            if (etInput.getText().toString().isEmpty()) {
+                LToast.show(activity, "Please input the entityId");
+            } else {
+                //TODO go to player
+            }
         }
     }
 }
