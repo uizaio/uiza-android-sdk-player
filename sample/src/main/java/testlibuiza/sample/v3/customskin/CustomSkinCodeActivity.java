a0fa8836bfbaa1b3753b9710e5f658b60edfbe86
Loitp <loitp@pateco.vn>
2018-09-28 13:40:34 +0700

    update framgia demo skin

--- sample/src/main/java/testlibuiza/sample/v3/customskin/CustomSkinCodeActivity.java
+++ sample/src/main/java/testlibuiza/sample/v3/customskin/CustomSkinCodeActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
 
 import testlibuiza.R;
 import testlibuiza.app.LSApplication;
