bbcbe7e59724efec0ebde7b77d4a9575f9334b0c
Loitp <loitp@pateco.vn>
2018-10-02 09:40:23 +0700

    remove dummy codes

--- sampletv/src/main/java/testlibuiza/app/LSApplication.java
+++ sampletv/src/main/java/testlibuiza/app/LSApplication.java
@@ -12,13 +12,6 @@ import vn.uiza.uzv3.util.UZUtil;
 public class LSApplication extends MultiDexApplication {
     private static LSApplication instance;
     private Gson gson;
-    /*private final String DF_DOMAIN_API = "teamplayer.uiza.co";
-    private final String DF_TOKEN = "uap-01e137ad1b534004ad822035bf89b29f-b9b31f29";
-    private final String DF_APP_ID = "01e137ad1b534004ad822035bf89b29f";
-    public static String entityIdDefaultVOD = "baf65548-e9f6-4ad1-be92-c6a44eb9d16f";
-    public static String entityIdDefaultLIVE = "1759f642-e062-4e88-b5f2-e3022bd03b57";
-    public static String metadataDefault0 = "53c2e63e-6ddf-4259-8159-cb43371943d1";
-    private final int env = Constants.ENVIRONMENT_PROD;*/
 
     private final String DF_DOMAIN_API = "veep.uiza.co";
     private final String DF_TOKEN = "uap-4f892058d35e46dd8a3a660481583b79-6a1319cb";
@@ -37,11 +30,8 @@ public class LSApplication extends MultiDexApplication {
         }
         //config activity transition default
         ActivityData.getInstance().setType(Constants.TYPE_ACTIVITY_TRANSITION_FADE);
-        Constants.setDebugMode(true);
-        //initWorkspace();
-        //UZUtil.initWorkspace(DF_DOMAIN_API, DF_TOKEN, DF_APP_ID, env, currentPlayerId);
+        Constants.setDebugMode(false);
         UZUtil.initWorkspace(this, DF_DOMAIN_API, DF_TOKEN, DF_APP_ID);
-        //AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
     }
 
     public Gson getGson() {
