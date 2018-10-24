e5c2bc874f00c898255581a080f714f788c4d015
Loitp <loitp@pateco.vn>
2018-10-24 11:40:52 +0700

    update core

--- uizacoresdk/src/main/java/uizacoresdk/view/rl/video/UZVideo.java
+++ uizacoresdk/src/main/java/uizacoresdk/view/rl/video/UZVideo.java
@@ -17,7 +17,6 @@ import android.util.Pair;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckedTextView;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
