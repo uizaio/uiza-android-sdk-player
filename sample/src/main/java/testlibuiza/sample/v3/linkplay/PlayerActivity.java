146c12672b2ad82649f6af1c2f7a8b1d56ad04cb
Loitp <loitp@pateco.vn>
2018-10-25 11:01:58 +0700

    update core add fucn init custom linkplay

--- sample/src/main/java/testlibuiza/sample/v3/linkplay/PlayerActivity.java
+++ sample/src/main/java/testlibuiza/sample/v3/linkplay/PlayerActivity.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import testlibuiza.R;
-import testlibuiza.app.LSApplication;
 import uizacoresdk.util.UZUtil;
 import uizacoresdk.view.rl.video.UZCallback;
 import uizacoresdk.view.rl.video.UZVideo;
@@ -56,15 +55,33 @@ public class PlayerActivity extends BaseActivity implements UZCallback {
         btPlay = (Button) findViewById(R.id.bt_play);
         uzVideo.setUZCallback(this);
 
-        etLinkPlay.setText("https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd");
-
-        //listLinkPlay.add("https://toanvk-live.uizacdn.net/893db5e8bb3943bfb12894fec56c8875-live/hi-uaqsv9as/manifest.mpd");
-        //listLinkPlay.add("http://112.78.4.162/drm/test/hevc/playlist.mpd");
-        //listLinkPlay.add("http://112.78.4.162/6yEB8Lgd/package/playlist.mpd");
-        //listLinkPlay.add("https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd");
+        findViewById(R.id.bt_0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                etLinkPlay.setText("https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd");
+            }
+        });
+        findViewById(R.id.bt_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                etLinkPlay.setText("http://yt-dash-mse-test.commondatastorage.googleapis.com/media/feelings_vp9-20130806-manifest.mpd");
+            }
+        });
+        findViewById(R.id.bt_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                etLinkPlay.setText("http://112.78.4.162/6yEB8Lgd/package/playlist.mpd");
+            }
+        });
+        findViewById(R.id.bt_3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                etLinkPlay.setText("https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8");
+            }
+        });
 
         LUIUtil.setLastCursorEditText(etLinkPlay);
-
+        findViewById(R.id.bt_0).performClick();
         btPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
