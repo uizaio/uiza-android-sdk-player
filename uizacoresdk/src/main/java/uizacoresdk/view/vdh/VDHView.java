5b3744f17f3707b90f7c2881c3ade5298bd256d9
Loitp <loitp@pateco.vn>
2019-03-12 10:28:49 +0700

    update vdhview

--- uizacoresdk/src/main/java/uizacoresdk/view/vdh/VDHView.java
+++ uizacoresdk/src/main/java/uizacoresdk/view/vdh/VDHView.java
@@ -16,6 +16,7 @@ import android.widget.LinearLayout;
 
 import uizacoresdk.R;
 import uizacoresdk.view.UZPlayerView;
+import vn.uiza.core.utilities.LLog;
 import vn.uiza.core.utilities.LScreenUtil;
 
 /*
@@ -415,6 +416,7 @@ public class VDHView extends LinearLayout {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        LLog.d(TAG, "onLayout " + state);
         if (state == State.BOTTOM ||
                 state == State.BOTTOM_RIGHT ||
                 state == State.BOTTOM_LEFT ||
