f40c27eb359131f651bafc83e5f9c4a40cbd819b
nguyen.huu.nam <proman112111@gmail.com>
2019-05-31 17:33:30 +0700

    [Improvement] Update API references from v4

---
+++ samplelivestream/src/main/java/com/uiza/module/samplelivestream/App.java
@@ -0,0 +1,22 @@
+package com.uiza.module.samplelivestream;
+
+import android.support.multidex.MultiDexApplication;
+
+import uizalivestream.util.UZUtil;
+import vn.uiza.core.common.Constants;
+
+public class App extends MultiDexApplication {
+    //TODO input information of your workspace
+    public static final String DF_DOMAIN_API = "teamplayer-api.uiza.co";
+    public static final String DF_TOKEN = "uap-01e137ad1b534004ad822035bf89b29f-b9b31f29";
+    public static final String DF_APP_ID = "01e137ad1b534004ad822035bf89b29f";
+    public static String entityIdDefaultLIVE_TRANSCODE = "ee375e25-5373-4b7e-8f5d-4a70eb539a5a";
+    public static String entityIdDefaultLIVE_NO_TRANSCODE = "aab8b32e-438a-4f86-8478-f82139de8902";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Constants.setDebugMode(false);
+        UZUtil.initWorkspace(this, Constants.API_VERSION_3, DF_DOMAIN_API, DF_TOKEN, DF_APP_ID);
+    }
+}
